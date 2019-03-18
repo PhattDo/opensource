7e7bf9698a966ff8904d2770da83947d42d50fa9
PhattDo <userisclown@gmail.com>
2019-03-18 19:42:38 +0700

    Layout

---
+++ Layouts/index.php
@@ -0,0 +1,11 @@
+<?php require_once __DIR__. "/Layouts/Header.php"; ?>
+    <!-- Breadcrumbs-->
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="#">Dashboard</a>
+        </li>
+        <li class="breadcrumb-item active">Trang Chủ Admin</li>
+    </ol>
+    <!-- Page Content -->
+    <h1>Chào Mừng Đến Với Trang Quản Trị Admin</h1>
+<?php require_once __DIR__. "/Layouts/Footer.php"; ?>
